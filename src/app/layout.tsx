3be4933a7fb3701b514fe1ffa27b8979ac08e698
Amalrajambali <amalrajambali123@gmail.com>
2026-06-10 15:01:34 +0530

    vercel analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
   title: 'Isra Churidars & Sarees | Isra Ethnic',
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className="font-body antialiased bg-background text-foreground min-h-screen">
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
